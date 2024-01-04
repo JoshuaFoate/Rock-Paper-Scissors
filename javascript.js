f9cc8e302224c35c0abdd08e89cfd3459a81874c
Joshua Foate <joshuafoate@gmail.com>
2024-01-04 13:59:21 -0600

    Created the game function in .js file.
    There is one bug where sometimes the result of a round does not get recorded.

--- javascript.js
+++ javascript.js
@@ -13,9 +13,9 @@ function getComputerChoice() {
     }
 }
 
-function singleRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Type Rock, Paper, or Scissors!")
+function singleRound() {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Type Rock, Paper, or Scissors!")
     if (playerSelection.toUpperCase() !== "ROCK" && playerSelection.toUpperCase() !== "PAPER" && playerSelection.toUpperCase() !== "SCISSORS") {
         alert("You need to type either rock, paper, or scissors!");
         singleRound();
@@ -26,13 +26,41 @@ function singleRound(playerSelection, computerSelection) {
         return;
     }
     else if(playerSelection.toUpperCase() === "ROCK" && computerSelection === "PAPER" || playerSelection.toUpperCase() === "PAPER" && computerSelection === "SCISSORS" || playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "ROCK") {
-        let result = "HAHAHA! " + computerSelection + " beats " + playerSelection.toUpperCase() + "!"
-        return console.log(result);
+        let message = "HAHAHA! " + computerSelection + " beats " + playerSelection.toUpperCase() + "!";
+        let result = 2;
+        console.log(message);
+        return result;    
     }
     else if(playerSelection.toUpperCase() === "ROCK" && computerSelection === "SCISSORS" || playerSelection.toUpperCase() === "PAPER" && computerSelection === "ROCK" || playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "PAPER") {
-        let result = "Good job! " + playerSelection.toUpperCase() + " beats " + computerSelection + "!"
-        return console.log(result);
+        let message = "Good job! " + playerSelection.toUpperCase() + " beats " + computerSelection + "!";
+        let result = 1;
+        console.log(message);
+        return result;
     }
 }
 
-console.log(singleRound());
+function game() {
+    let i;
+    let j;
+    for (i = 0, j = 0; i < 3 && j < 3;) {
+        let result = singleRound();
+        if (result === 1) {
+            console.log("You won this round!");
+            ++i;
+            console.log("Score: " + i + " : " + j);
+        }
+        else if (result === 2) {
+            console.log("You lost this round !");
+            ++j;
+            console.log("Score: " + i + " : " + j);
+        }
+    }
+    if (i === 3) {
+        console.log("YOU WON!");
+    }
+    else if (j === 3) {
+        console.log("YOU LOST!");
+    }
+}
+
+game();
